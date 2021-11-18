1fd78f625f74488952cf7e3ccd5cd76413a30cfd
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2021-11-18 13:10:02 +0530

    create view folder

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const {PORT} = require('./config');
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
+});

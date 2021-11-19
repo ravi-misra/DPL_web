f0276a648792874c503acfc515a086a0a81346c0
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2021-11-19 21:49:16 +0530

    create initial structure

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
 app.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`);
 });

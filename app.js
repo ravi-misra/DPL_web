3facaaa152930d45d55c22b2fca1dd59f037ba6c
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2021-11-22 07:47:51 +0530

    basic test demo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
@@ -12,14 +13,22 @@ const {PORT} = require('./config');
 const app = express();
 
 app.engine('ejs', ejsMate);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(methodOverride('_method'));
+app.use(flash());
 
 
+app.get('/', (req, res) => {
+    res.render('users/shift-schedule-form.ejs');
+})
 
-app.listen(PORT, () => {
+app.listen(PORT, '10.130.64.23', () => {
     console.log(`Listening at port ${PORT}`);
 });

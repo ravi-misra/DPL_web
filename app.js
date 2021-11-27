ca3e81cbf673333f80c2f7b3544dc42106da0c3a
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2021-11-28 00:00:17 +0530

    setup express-session middleware

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const {PORT, LOCAL_HOST, PLCI_HOST} = require('./config');
+const {PORT, LOCAL_HOST, PLCI_HOST, DB_URL} = require('./config');
 
 // const host = PLCI_HOST;
 const host = LOCAL_HOST;
 
 const app = express();
 
+dbConnect().catch(err => console.log(err));
+async function dbConnect() {
+    await mongoose.connect(DB_URL);
+    console.log('Database connected.');
+}
+
 app.engine('ejs', ejsMate);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const store = MongoStore.create({ 
+    clientPromise: mongoose.connection.asPromise().then(() => mongoose.connection.getClient()),
+    touchAfter: 24*3600 // time period in seconds
+});
 
-app.use(cookieParser());
+const sessionConfig = {
+    store,
+    secret: 'thisisnotagoodsecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000*60*60*24*7, // time period in miliseconds
+    }
+}
+
+app.use(session(sessionConfig));
+// app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'));
@@ -29,7 +52,13 @@ app.use(flash());
 
 
 app.get('/', (req, res) => {
-    res.render('users/shift-schedule-form.ejs');
+    // if(req.session.count) {
+    //     req.session.count += 1;
+    // } else {
+    //     req.session.count = 1;
+    // }
+    // res.send(`You have viewed ${req.session.count} times.`);
+    res.render('users/shift-schedule-form.ejs'); 
 })
 
 app.listen(PORT, host, () => {
--- config.js
+++ config.js
@@ -4,10 +4,12 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     LOCAL_HOST: process.env.LOCAL_HOST,
-    PLCI_HOST: process.env.PLCI_HOST
+    PLCI_HOST: process.env.PLCI_HOST,
+    DB_URL: process.env.DB_URL
 }
 
 // .env file contents
 // PORT=3000
 // LOCAL_HOST="127.0.0.1"
 // PLCI_HOST="10.130.64.23"
+// DB_URL="mongodb://localhost:27017/yelp-camp"

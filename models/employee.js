3f31ff96e0f15ed672d3b9afeb7e30b7d0578533
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2021-12-01 21:37:58 +0530

    model creation start

---
+++ models/employee.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+    pn: {
+        type: String,
+        required: true
+    }
+})
+
+const Employee = mongoose.model('Employee', employeeSchema);

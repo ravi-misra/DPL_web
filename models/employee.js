619a579911d12c9f82bd7109bf17b595759b7c93
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2021-12-02 21:56:44 +0530

    make employee model

--- models/employee.js
+++ models/employee.js
@@ -4,8 +4,34 @@ const Schema = mongoose.Schema;
 const employeeSchema = new Schema({
     pn: {
         type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
         required: true
-    }
+    },
+    dept_code: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['M', 'F']
+    },
+    dob: Date,
+    doj: Date,
+    mobile: Number,
+    alternate_contacts: [Number],
+    intercom: Number,
+    qtr_no: String,
+    blood_grp: String,
+    password: String
 })
 
 const Employee = mongoose.model('Employee', employeeSchema);

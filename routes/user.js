3d46acb6b1bd5dd50d280a6b01438fd5decd9014
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2021-12-15 13:25:07 +0530

    route drafting

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+//===========USERS==========================
+// /mydpl  -> (individual homepage as per profile)
+// /mydpl/schedule  -> (update public shift schedule)
+// /mydpl/profile  -> (update personal profile)
+// /mydpl/attendance  -> (update casual attendance)
+
+//===========Plant Dashboards======================
+// /mydpl/plant  -> (view plant dashboards)
+
+
+//============Contract=========================
+// /mydpl/contract -> (view date wise attendance)

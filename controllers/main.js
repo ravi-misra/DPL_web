184b4ba0b5886bbbd7ddca494dcd6d2223a765c6
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2021-12-26 13:46:27 +0530

    setup controllers

---
+++ controllers/main.js
@@ -0,0 +1,18 @@
+const Employee = require('../models/employee');
+
+module.exports.renderLogin = (req, res) => {
+    res.render('users/login');
+}
+
+module.exports.login = (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res) => {
+    req.logout();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+}

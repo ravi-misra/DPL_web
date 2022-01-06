350a1ddace08583401883869e9ddb0b897faae07
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2022-01-06 22:56:11 +0530

    error template

--- controllers/main.js
+++ controllers/main.js
@@ -10,6 +10,7 @@ module.exports.login = (req, res) => {
         req.session.cookie.maxAge = 1000*60*60*24*30;
     }
     req.flash('success', 'Welcome!');
+    console.log(error);
     const redirectUrl = req.session.returnTo || '/home';
     delete req.session.returnTo;
     res.redirect(redirectUrl);

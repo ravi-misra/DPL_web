2eebc04c0f12a1ed306971edf500b569980814a1
ravi-misra <52475972+ravi-misra@users.noreply.github.com>
2022-05-19 13:14:42 +0530

    add gap1 route

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -1,4 +1,5 @@
 const Gap2AnodeData = require("../models/gap2AnodeData");
+const Gap1AnodeData = require("../models/gap1AnodeData");
 const { addMinutes } = require("date-fns");
 
 module.exports.saveGap2AnodeData = async (req, res) => {
@@ -10,6 +11,24 @@ module.exports.saveGap2AnodeData = async (req, res) => {
             };
             let update = obj;
             let doc = await Gap2AnodeData.findOneAndUpdate(filter, update, {
+                new: true,
+                upsert: true,
+            });
+            await doc.save();
+        }
+    }
+    res.status(200).send("ok");
+};
+
+module.exports.saveGap1AnodeData = async (req, res) => {
+    if (req.body.length) {
+        for (let obj of req.body) {
+            obj["DateAndTime"] = addMinutes(new Date(obj["DateAndTime"]), 330);
+            let filter = {
+                DateAndTime: obj["DateAndTime"],
+            };
+            let update = obj;
+            let doc = await Gap1AnodeData.findOneAndUpdate(filter, update, {
                 new: true,
                 upsert: true,
             });
--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const apiController = require("../controllers/api_controller");
 const catchAsync = require("../utils/catchAsync");
 
 router.route("/ca/gap2").post(catchAsync(apiController.saveGap2AnodeData));
+router.route("/ca/gap1").post(catchAsync(apiController.saveGap1AnodeData));
 
 module.exports = router;
